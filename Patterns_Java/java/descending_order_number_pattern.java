8f878c60d438ce9564810ceead6739367d497244
Ujjwal Mishra <ujjwalmishra3131@gmail.com>
2025-01-26 21:46:23 +0530

    dsa patterns

---
+++ Patterns_Java/java/descending_order_number_pattern.java
@@ -0,0 +1,18 @@
+package Patterns_Java.java;
+
+public class descending_order_number_pattern
+{
+    public static void main(String[] args)
+    {
+       // int num ;
+        for (int i = 5 ; i>=1 ; i--)
+        {
+            for (int j = i ; j>=1 ; j-- )
+            {
+                System.out.print(" "+j);
+            }
+            System.out.println("\n");
+        }
+
+    }
+}
